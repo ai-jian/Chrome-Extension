7baa341d3841ef634b0485fcecde58ee90d1af39
Ai Jian <53556553+meowwwowo@users.noreply.github.com>
2022-01-23 16:48:57 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 let myLeads = []
 const inputEl = document.querySelector('#input-el')
 const inputBtn = document.querySelector('#input-btn')
+const tabBtn = document.querySelector('#tab-btn')
+const deleteBtn = document.querySelector('#delete-btn')
 const ulEl = document.querySelector('#ul-el')
 
-// localStorage.setItem("myName", "Ai")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+console.log(leadsFromLocalStorage)
 
-// let name = localStorage.getItem('myName')
-// console.log(name)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-localStorage.clear()
 
+tabBtn.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-inputBtn.addEventListener('click', () => {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    renderLeads()
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads() {
+function render(leadsArr) {
 
     let listItems = ''
-    for (i = 0; i < myLeads.length; i++) {
-        // ulEl.innerHTML += `<li> ${myLeads[i]}</li> `
+    for (i = 0; i < leadsArr.length; i++) {
+        // or ulEl.innerHTML += `<li> ${leadsArr[i]}</li> `
         listItems += `
         <li>
-            <a href='http://${myLeads[i]}' target='_blank'>
-                ${myLeads[i]}
+            <a href='http://${leadsArr[i]}' target='_blank'>
+                ${leadsArr[i]}
             </a>
         </li> 
         `
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', () => {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+
+inputBtn.addEventListener('click', () => {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    console.log(localStorage.getItem('myLeads'))
+    render(myLeads)
+})
